7a2973f5433a28019e527554c5223a1a2fc2ae60
André Diego Piske <andrepiske@gmail.com>
2016-06-14 10:35:27 -0300

    Add atom keymap file

--- install.sh
+++ install.sh
@@ -4,4 +4,6 @@ cp -f ./vimrc ~/.vimrc
 cp -Rf ./vim ~/.vim
 echo "Installing gemrc files"
 cp -f ./gemrc ~/.gemrc
+echo "Installing atom files"
+cp -f ./atom/keymap.cson ~/.atom/keymap.cson
 echo "Done"
